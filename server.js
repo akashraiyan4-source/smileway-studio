47432a7e8be85370ca55b11eb5901c032766ec7d
Akash Raiyan <akashraiyan4@gmail.com>
2026-09-12 16:30:13 +0600

    Fix static path for root video files

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static frontend files from 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // API Routes
 app.use('/api/leads', leadsRouter);
